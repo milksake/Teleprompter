2cece84c0e577829a1fecc95189f7a0a643f797d
joaquin30 <46060104+joaquin30@users.noreply.github.com>
2024-01-27 10:42:19 -0500

    TTS v2

--- TTS/tts.gd
+++ TTS/tts.gd
@@ -1,18 +1,12 @@
 extends Timer
 
 # fuente: https://tmpxyz.itch.io/auditionroom-webgl
-@onready var bips := [$bip1, $bip2, $bip3, $bip4]
-var end := false
+@onready var bips : Array[AudioStreamPlayer] = [$bip1, $bip2, $bip3, $bip4]
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	connect("timeout", play_bip)
 
 func play_bip():
-	var ix = randi() % (bips.size() + 1)
+	var ix := randi() % (bips.size() + 1)
 	if ix < bips.size():
 		bips[ix].play()
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float):
-	pass
